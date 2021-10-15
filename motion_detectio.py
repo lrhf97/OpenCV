8d35543eb91ab597e91f2e41f70fd1074d240046
lrhf97 <awstclair@gmail.com>
2021-10-15 17:23:10 -0400

    bounding boxes on the movement

--- motion_detectio.py
+++ motion_detectio.py
@@ -17,14 +17,29 @@ while True:
 
     delta_frame=cv2.absdiff(first_frame,gray_frame)
 
-    thresh_frame=cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
+    #threshold to split it up and down based on an "amount different".  In this case '30
+    thresh_frame=cv2.threshold(delta_frame, 35, 255, cv2.THRESH_BINARY)[1]
+
+    #dilate blends together some of the bright spots that are close together
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+
+    (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # filter out smaller items. if the countour has less than 1000 pix then move on to the next one.
+    for contour in cnts:
+        if cv2.contourArea(contour) <1000:
+            continue
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w,y+h),(0,255,0),3)
 
     cv2.imshow("Gray Frame",gray_frame)
     cv2.imshow("Delta Frame",delta_frame)
     cv2.imshow("Threshold Frame",thresh_frame)
+    cv2.imshow("Color Framqe",frame)
 
 
-    key = cv2.waitKey(1)
+
+    key = cv2.waitKey(1) 
 
     if key == ord('q'):
         break
